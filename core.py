271e2281c0ab579955e73152341541af1357dd14
Cortexan <gcdodwell@gmail.com>
2024-04-23 03:00:24 +0100

    pep8 formatting

--- core.py
+++ core.py
@@ -6,8 +6,8 @@ from settings import *
 from stimuli import Shape, Fix
 
 #%% - creating stimuli
-#region - click carrot to expand
-# creating all of the stimuli - these are all instances of the Shape class, which is defined in stimuli.py
+# region click carrot to expand
+# creating all the stimuli - these are all instances of the Shape class, which is defined in stimuli.py
 # the Shape class requires a shape name (string) and a scale (float) as arguments
 # you can create multiple instances of the same shape, as long as you give them different variable names.
 # then, each instance can be drawn and displayed on the screen independently.
@@ -15,7 +15,7 @@ from stimuli import Shape, Fix
 # the scale is a float that will be multiplied by the size of the shape in degrees visual angle.
 # all shapes have an equal surface area for a given scale, so they will all be the same size on the screen.
 # you can change the size of the shapes by changing the scale value.
-#endregion
+# endregion
 
 square      = Shape(shape = 'square',   scale = shape_dva)
 diamond     = Shape(shape = 'diamond',  scale = shape_dva)
@@ -34,17 +34,17 @@ shapes = [square, diamond, triangle, circle, cross, hexagon, rectangle, oval, ga
 #%% - creating radial grid positions for each ring
 
 # # create radial grid positions for each ring -- not in use yet
-# r1_postions = radGrid(n_stim_r1, r1_radius)
-# r2_postions = radGrid(n_stim_r2, r2_radius)
+# r1_positions = radGrid(n_stim_r1, r1_radius)
+# r2_positions = radGrid(n_stim_r2, r2_radius)
 
 #%% - setup colors
-#region
+# region click carrot to expand
 # select colors from color_set (defined in settings.py) - 
 # I'm using list comprehension to get a different color value from the dictionary for each shape,
 # but you could also just list them out manually, calling each color (or 'key') by name.
-# the dictionary is a convenent way to be able to refer to each colour by a name rather than a number
+# the dictionary is a convenient way to be able to refer to each colour by a name rather than a number
 # (e.g. color_set[0] vs color_set['red']). There are various alternative ways this could be set up. 
-#endregion
+# endregion
 colors = [color for color in color_set.values()]
 colors = colors[:len(shapes)+1]
 
@@ -71,7 +71,8 @@ for idx, shape in enumerate(shapes):
     # the "setColor" method sets the color of the shape -
     # you can similarly set various parameters such as orientation, size, color, position, opacity, etc.
     shape.setColor(colors[idx])
-    # draw the shape on screen for a set duration of time (defined in "flips", which have the duration of a single frame)
+    # draw the shape on screen for a set duration of time
+    # (defined in "flips", which have the duration of a single frame)
     for frame in range(isi_dur):
         # the "draw" method draws the shape on the screen, but only for the next flip.
         shape.draw()
